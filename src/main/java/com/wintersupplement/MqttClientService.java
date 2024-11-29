2414f25e7dddd9959875aa5e7d7f8101d98a4888
A-dith-ya <awedithya@gmail.com>
2024-11-29 08:26:01 -0800

    Implement parsing of incoming and outgoing messages using ObjectMapper

--- src/main/java/com/wintersupplement/MqttClientService.java
+++ src/main/java/com/wintersupplement/MqttClientService.java
@@ -2,15 +2,21 @@ package com.wintersupplement;
 
 import org.eclipse.paho.client.mqttv3.*;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wintersupplement.model.WinterSupplementInput;
+import com.wintersupplement.model.WinterSupplementOutput;
+
 public class MqttClientService {
     private String topicId;
     private IMqttClient client;
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     public MqttClientService(String topicId) {
         this.topicId = topicId;
     }
 
-    public void connect() throws MqttException {
+     public void connect() throws MqttException {
         client = new MqttClient("tcp://test.mosquitto.org:1883", this.topicId);
 
         MqttConnectOptions options = new MqttConnectOptions();
@@ -18,9 +24,24 @@ public class MqttClientService {
         options.setCleanSession(true);
         options.setConnectionTimeout(10);
         client.connect(options);
+        System.out.println("Connected to MQTT broker");
 
         client.subscribe("BRE/calculateWinterSupplementInput/" + this.topicId, (topic, msg) -> {
-            System.out.println(new String(msg.getPayload()));
+            String payload = new String(msg.getPayload());
+            System.out.println("Received message: " + payload);
+            
+            WinterSupplementInput winterSupplementInput = objectMapper.readValue(payload, WinterSupplementInput.class);
+            publishMessage();
         });
+        System.out.println("Subscribed to topic: " + this.topicId);
+    }
+
+    public void publishMessage() throws MqttException, JsonProcessingException  {
+        WinterSupplementOutput winterSupplementOutput = new WinterSupplementOutput(this.topicId, false, 0, 0, 0);
+        
+        String outputJson = objectMapper.writeValueAsString(winterSupplementOutput);
+        MqttMessage msg = new MqttMessage(outputJson.getBytes());
+        client.publish("BRE/calculateWinterSupplementOutput/" + this.topicId, msg);
+        System.out.println("Published message: " + outputJson);
     }
 }
